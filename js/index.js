aae243f712e8b156ae3a78dd4c852f129dd9a440
Rhys Walsh-Tindall <rhys.walsh-tindall@cultureamp.com>
2018-09-09 11:41:33 +1000

    Multi-language changing implementation

---
+++ js/index.js
@@ -0,0 +1,5 @@
+var languageRedirect = function() {
+	// hack to only show for polish :(
+	document.cookie = "language=po";
+	window.location = 'index.po.html';
+}
--- js/navigation.js
+++ js/navigation.js
@@ -1,37 +1,54 @@
 goTo = function(location) {
+    languageCode = getLanguage();
     switch(location) {
 	    case 'index':
-	        window.location = 'index.en.html'
+	        window.location = 'index.' + languageCode + '.html';
 	        break;
         case 'resident':
-        	window.location = 'index.en.html'
+        	window.location = 'index.' + languageCode + '.html';
         	break;
     	case 'visitor':
-    		window.location = 'visitor.en.html'
+    		window.location = 'visitor.' + languageCode + '.html';
     		break;
 	    case 'about':
-	        window.location = 'about.en.html'
+	        window.location = 'about.' + languageCode + '.html';
 	        break;
         case 'on_this_day':
-        	window.location = 'on_this_day.en.html'
+        	window.location = 'on_this_day.' + languageCode + '.html';
         	break;
         case 'council':
-            window.location = 'council.en.html'
+            window.location = 'council.' + languageCode + '.html';
             break;
     	case 'sports':
-    		window.location = 'sports.en.html'
+    		window.location = 'sports.' + languageCode + '.html';
     		break;
     	case 'census':
-    		window.location = 'census.en.html'
+    		window.location = 'census.' + languageCode + '.html';
     		break;
     	case 'afl':
-    		window.location = 'afl.en.html'
+    		window.location = 'afl.' + languageCode + '.html';
     		break;
         case 'events':
-            window.location = 'events.en.html'
+            window.location = 'events.' + languageCode + '.html';
             break;
 	    default:
-	        window.location = 'index.en.html'
+	        window.location = 'index.' + languageCode + '.html';
 	        break;
 	}
+}
+
+getLanguage = function() {
+    name = "language="
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "en";
 }
